20f632891855e9ce7b27619c3128bf920233a19d
ezaborszky <ede.zaborszky@gmail.com>
2025-05-25 12:27:19 +0200

    day6

--- six/main.cpp
+++ six/main.cpp
@@ -1,7 +1,10 @@
+#include <algorithm>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string.h>
+#include <unordered_map>
 #include <vector>
 using ruleSet = std::pair<int, int>;
 using sequenceSet = std::vector<int>;
@@ -12,7 +15,7 @@ int main(int argc, char *argv[]) {
   std::vector<sequenceSet> sequences;
   bool inputSwitch = true;
   std::string line = "";
-
+  int sum = 0;
   while (std::getline(file, line)) {
     std::stringstream stream(line);
     std::string firstToken, secondToken;
@@ -39,16 +42,33 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  std::unordered_map<int, std::vector<int>> notBefore;
+
   for (auto a : rules) {
-    std::cout << "{ " << a.first << " | " << a.second << " } \n";
+    notBefore[a.second].push_back(a.first);
   }
 
-  for (auto a : sequences) {
-    std::cout << "[ ";
-    for (auto b : a) {
-      std::cout << b << ",";
+  for (size_t index = 0; index < sequences.size(); ++index) {
+    bool isLegit = true;
+    for (size_t i = 1; i < sequences[index].size(); ++i) {
+
+      for (size_t subindex = 0; subindex < i; ++subindex) {
+        for (const auto &num : rules) {
+          if (sequences[index][i] == num.first &&
+              sequences[index][subindex] == num.second) {
+            isLegit = false;
+            sequences[index][subindex] = num.first;
+            sequences[index][i] = num.second;
+          }
+        }
+      }
     }
-    std::cout << "] \n";
+    if (!isLegit) {
+      int middle = sequences[index][sequences[index].size() / 2];
+      sum += middle;
+    }
   }
+
+  std::cout << sum << "\n";
   return 0;
 }
